abdfd8142e56d436a736c804680348089c61ee31
Alex Kulcsar <38046796+alex-kulcsar@users.noreply.github.com>
2024-11-16 16:12:12 -0500

    Update block annotation.

--- wordlists.ts
+++ wordlists.ts
@@ -3,7 +3,7 @@ namespace WordLists {
     //% block="Empty Filter" weight=0
     export let zEmptyFilter: Bloom = new Bloom(null)
     
-    //% block
+    //% block="is word $word in filter $filter"
     export function isWordInFilter(filter: Bloom, word: string): boolean {
         return filter.findWord(word)
     }
