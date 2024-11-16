23485f57e6b8ef102e1db3c1ba03e042861b2f79
Alex Kulcsar <38046796+alex-kulcsar@users.noreply.github.com>
2024-11-16 15:43:36 -0500

    Update empty set variable name.

--- wordlists.ts
+++ wordlists.ts
@@ -1,6 +1,7 @@
 namespace WordLists {
     //% fixedInstance
-    export let EmptyFilter: Bloom = new Bloom(null)
+    //% block="z Empty Filter"
+    export let zEmptyFilter: Bloom = new Bloom(null)
     
     //% block
     export function isWordInFilter(filter: Bloom, word: string): boolean {
